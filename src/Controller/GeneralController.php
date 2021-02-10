c224a8217a73434f5312a91d900f3f5414c0e2e4
Chirace <alexis.chirat@etud.u-picardie.fr>
2021-02-10 15:12:54 +0100

    Début BDD et erreur 404

--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -7,4 +7,9 @@ class GeneralController extends AbstractController {
    public function accueil() {
        return $this->render('general/accueil.html.twig');
    }
+
+   public function erreur($url) {
+        throw $this->createNotFoundException('Erreur 404 - Page introuvable');
+        return $this->render('general/error404.html.twig', array('url' => $url));
+    } 
 }
