eb39c65b23a39b656b3e695ff03ecf7d1eae1b99
Chirace <alexis.chirat@etud.u-picardie.fr>
2021-02-17 17:04:26 +0100

    Ajout routes et création pages pour ResponsableUE et secretariat et début CSS

--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -8,6 +8,34 @@ class GeneralController extends AbstractController {
        return $this->render('general/accueil.html.twig');
    }
 
+   public function homeRue(){
+       return $this->render('responsableUE/accueil.html.twig');
+   }
+
+   public function setting(){
+       return $this->render('responsableUE/setting.html.twig');
+   }
+
+   public function listStudents(){
+       return $this->render('responsableUE/students.html.twig');
+   }
+
+   public function listTutors(){
+       return $this->render('responsableUE/tutors.html.twig');
+   }
+
+   public function assignTutor(){
+       return $this->render('responsableUE/assignTutor.html.twig');
+   }
+
+   public function addSheet(){
+       return $this->render('secretariat/addSheet.html.twig');
+   }
+
+   public function addListStudent(){
+    return $this->render('secretariat/addListStudent.html.twig');
+   }
+
    public function erreur($url) {
         throw $this->createNotFoundException('Erreur 404 - Page introuvable');
         return $this->render('general/error404.html.twig', array('url' => $url));
